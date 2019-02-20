f959be4b7d99a975280333c525c70629cd6f7206
Shashankmishra998 <shashank.mishra_ccv17@gla.ac.in>
2019-02-20 11:09:14 +0530

    add comments

--- src/MAIN/node.java
+++ src/MAIN/node.java
@@ -1,7 +1,15 @@
 package MAIN;
 
 public class node {
+    // commit Ctrl+k
+    // push Ctrl+shift+k
      private int data;
 
+    public int getData() {
+        return data;
+    }
 
+    public void setData(int data) {
+        this.data = data;
+    }
 }
