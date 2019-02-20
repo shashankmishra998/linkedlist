f3f2da0cb5c6bca57ebc22ce6477c96d270aa5d7
Shashankmishra998 <shashank.mishra_ccv17@gla.ac.in>
2019-02-20 11:23:18 +0530

    add constructor

--- src/MAIN/node.java
+++ src/MAIN/node.java
@@ -8,6 +8,26 @@ public class node {
      private int data;
      private Node next;
 
+    public Node getNext() {
+        return next;
+    }
+
+    public node(int data) {
+        this.data = data;
+    }
+
+    public node(Node next) {
+        this.next = next;
+    }
+
+    public node() {
+    }
+
+    public void setNext(Node next) {
+
+    }
+
+
     public int getData() {
         return data;
     }
