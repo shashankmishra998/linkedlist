7f4386ab4e9ba4ff6cd6fa797c21f958ed840575
Shashankmishra998 <shashank.mishra_ccv17@gla.ac.in>
2019-02-26 15:01:57 +0530

    Singly Linked List third commit

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -21,10 +21,6 @@ public class SinglyLinkedList {
             this.data = data;
         }
 
-        public Node(Node next) {
-            this.next = next;
-        }
-
         public Node(int data) {
             this.data = data;
             this.next = null;
@@ -40,4 +36,39 @@ public class SinglyLinkedList {
             this.next = next;
         }
     }
+    private Node head = null;
+    private int size = 0;
+
+    public static void main(String[] args) {
+        SinglyLinkedList linkedlist = new SinglyLinkedList();
+        System.out.println(linkedlist);
+        for(int i = 0;i<5;i++)
+        {
+            linkedlist.insertHead(i+1);
+        }
+         linkedlist.insertHead(42);
+        System.out.println(linkedlist);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        Node temp = this.head;
+        while(temp!=null) {
+            response.append(temp.getData());
+            temp = temp.next;
+        }
+        response.append("]");
+        return super.toString();
+    }
+
+    private void insertHead(int data){
+        Node newNode = new Node(data,this.head);
+        this.head = newNode;
+        size++;
+}
+
+
+
 }
